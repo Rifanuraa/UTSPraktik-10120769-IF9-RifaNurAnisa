1f558ed2aeb2edd1dcb5b56a752bcf4e2e6f5344
Rifa nur anisa <rifanura12@gmail.com>
2023-05-25 10:51:26 +0700

    UTS Praktik

---
+++ app/src/main/java/com/example/utspraktik_10120769_if9_rifanuranisa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.utspraktik_10120769_if9_rifanuranisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
